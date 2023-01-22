774053e144f84630e5a4e45380755747b454200f
FearGod12 <onyenikechukwudi@gmail.com>
2023-01-22 21:21:16 +0100

    new

--- _footer.php
+++ _footer.php
@@ -1,6 +1,23 @@
 <!-- 
     # Footer module --
 -->
+<div class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <p>Copyright © Fawzy Hotel and Event Planners 2023LTD.</p>
+            </div>
+            <div class="col">
+                <p>Contact Us: Block vii, plot 1,2,3 Adewumi layout, off Akinyemi Way, Ring Road, Ibadan. <br> Email: Fawzyhotel@gmail.com <br>Tel. 08105479934</p>
+            </div>
+            <div class="col">
+                <p>Terms and Conditions</p>
+            </div>
+        </div>
+    </div>
+</div>
+    <script src="script.js"></script>
+  </body>
 
 </body>
 </html>
